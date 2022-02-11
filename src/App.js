a21df8df6cea1ec03515666d77747abf55980dfe
unknown <amaechiizu@gmail.com>
2022-02-11 08:38:44 +0100

    seventh deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import NavBar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-       <Router basename="/">
+       <Router basename="/FinsweetWebflowDesignnn">
       <TestimonyContextProvider>
       <NavBar/>
         <Switch>
